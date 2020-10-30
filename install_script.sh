520abb2b0527a067776212d2c0585f763161eedd
Johan Sjöblom <sjoblomj@student.chalmers.se>
2020-10-30 16:45:11 +0100

    Script will now install git and download itself 😣

--- install_script.sh
+++ install_script.sh
@@ -1,8 +1,11 @@
 # TODO: Present user with bash-friendly options
 
 sudo pacman -Suy
-sudo pacman -S python-pip
+sudo pacman -S python-pip git
 pip install curses-menu
 
-python find_scripts.py
+mkdir -p ~/tools
+git clone https://github.com/sjoblomj/arch-install-scripts ~/tools
+cd ~/tools/arch-install-scripts
 
+python find_scripts.py
