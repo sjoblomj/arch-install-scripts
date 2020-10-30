e29e33fd7d2a9cc7b423fcd64e09e84a50533f49
Johan Sjöblom <sjoblomj@student.chalmers.se>
2020-10-31 00:49:52 +0100

    Updated utils ♻️

--- 0_utils/run.py
+++ 0_utils/run.py
@@ -1,11 +1,42 @@
 from cursesmenu import *
 from cursesmenu.items import *
+from shutil import which
+import subprocess
+import os
 
-menu = CursesMenu("Install Utils?")
-item_a = CommandItem("tree", "sudo pacman --needed -S tree")
-item_b = CommandItem("git",  "sudo pacman --needed -S git")
+programs = {
+  "tree": "Draws recursive file tree",
+  "ranger": "Terminal file manager",
+  "thunar": "GUI file manager",
+  "evince": "PDF-viewer",
+  "chromium": "Web browser",
+  "unzip": "Unzips files",
+  "task": "Task manager"
+}
 
-menu.append_item(item_a)
-menu.append_item(item_b)
-menu.show()
+def install(program):
+    subprocess.run(['sudo', 'pacman', '-S', '--needed', program])
 
+def get_programs_not_installed():
+    def is_not_installed(program):
+        return which(program) is None
+    return list(filter(is_not_installed, programs))
+
+def menu_fun():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    programs_not_installed = get_programs_not_installed()
+    def add_desc(program):
+        return program + ":  " + programs[program]
+
+    progs = map(add_desc, programs_not_installed)
+    if len(programs_not_installed) > 0:
+        menu = SelectionMenu(progs, "Install programs?")
+        menu.show()
+        menu.exit()
+        if menu.selected_option < len(programs_not_installed):
+            install(programs_not_installed[menu.selected_option])
+            menu_fun()
+    quit()
+
+menu_fun()
