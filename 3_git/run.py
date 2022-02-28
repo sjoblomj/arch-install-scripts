c1a010b8364d951d6960a45728f2e0360d23572a
sjoblomj <sjoblomj@student.chalmers.se>
2022-02-28 23:35:55 +0100

    Updating git alias 👿

--- 3_git/run.py
+++ 3_git/run.py
@@ -3,6 +3,21 @@ from cursesmenu.items import *
 from pathlib import Path
 import subprocess
 
+def add_line_to_file(f, line):
+    subprocess.Popen("echo " + line, stdout=f, stderr=f, shell=True)
+
+def add_alias(alias):
+    alias_file = str(Path.home()) + '/.zshaliases'
+    with open(alias_file, 'a+') as f:
+       add_line_to_file(f, alias)
+    zshrc_file = str(Path.home()) + '/.zshrc'
+    with open(zshrc_file, 'a+') as f:
+        f.seek(0)
+        content = f.read()
+        source_text = "source ~/.zshaliases"
+        if source_text not in content:
+            add_line_to_file(f, source_text)
+
 def install_git():
     subprocess.run(['sudo', 'pacman', '-S', '--needed', 'git'])
     subprocess.run(['sudo', 'pacman', '-S', '--needed', 'diff-so-fancy'])
@@ -13,10 +28,7 @@ def install_git():
     subprocess.run(["git config --global alias.l \"log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(red)- %an%C(reset)%C(bold yellow)%d%C(reset)'\""], shell=True)
     subprocess.run(["git config --global alias.l2 \"log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(red)- %an%C(reset)'\""], shell=True)
 
-    alias_file = str(Path.home()) + '/.zshaliases'
-    with open(alias_file, 'a+') as f:
-        subprocess.Popen("echo alias gits=\\'git status\\'", stdout=f, stderr=f, shell=True)
-
+    add_alias("alias gits=\\'git status\\'")
     print("Enter name (to use globally for git)")
     name = input()
     print("Enter email (to use globally for git)")
