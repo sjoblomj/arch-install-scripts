33f6d0281b9fcf2045ee2d58083d9375c8526914
Johan Sjöblom <sjoblomj88@gmail.com>
2022-03-03 19:26:08 +0100

    Using external program for setting screen resolution 🗺️

--- 9_config/change_resolution.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-targetfile="$HOME/.config/xfce4/xfconf/xfce-perchannel-xml/displays.xml"
-resolution="2047x1152"
-if [ -f $targetfile ]; then
-	sed -iE "s|<property name=\"Resolution\" type=\"string\" value=\".*\"/>|<property name=\"Resolution\" type=\"string\" value=\"$resolution\"/>|g" $targetfile
-fi
-
--- 9_config/run.py
+++ 9_config/run.py
@@ -1,32 +1,37 @@
 from cursesmenu import *
 from cursesmenu.items import *
 from pathlib import Path
+from shutil import which
 import subprocess
 
 has_run_keybindings = False
 has_run_resolution  = False
+resolution_program = "xfce4-display-settings"
 
 def openbox_config_file_exists():
+    global has_run_keybindings
     file = Path(str(Path.home()) + "/.config/openbox/rc.xml")
     return file.is_file() and not has_run_keybindings
 
 def resolution_config_file_exists():
-    file = Path(str(Path.home()) + "/.config/xfce4/xfconf/xfce-perchannel-xml/displays.xml")
-    return file.is_file() and not has_run_resolution
+    global has_run_resolution
+    return which(resolution_program) is not None and not has_run_resolution
 
 def add_keybindings():
     subprocess.run(['sh', '-c', '9_config/add_keybindings.sh'])
 
 def change_resolution():
-    subprocess.run(['sh', '-c', '9_config/change_resolution.sh'])
+    subprocess.run([resolution_program])
 
 
 def menu_fun():
+    global has_run_keybindings
+    global has_run_resolution
     lst = []
     if openbox_config_file_exists():
         lst.append("Add keybindings to ~/.config/openbox/rc.xml  (Will cause screen to flicker)")
     if resolution_config_file_exists():
-        lst.append("Change resolution in ~/.config/xfce4/xfconf/xfce-perchannel-xml/displays.xml  (Reboot needed to take effect)"))
+        lst.append("Change resolution  (Open " + resolution_program + ")")
 
     if len(lst) > 0:
         menu = SelectionMenu(lst, "Update system configuration?")
