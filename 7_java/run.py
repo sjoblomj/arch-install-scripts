ae63c410666efeed7692b3d177eed13880b4d030
Johan Sjöblom <sjoblomj@student.chalmers.se>
2022-03-03 13:02:06 +0100

    Adding mvntree 🌳

--- 7_java/run.py
+++ 7_java/run.py
@@ -4,23 +4,36 @@ from pathlib import Path
 import subprocess
 import re
 
-maven = "maven"
+installed_programs = []
+maven   = "maven    (Java build automation tool)"
+mvntree = "mvntree  (mvn dependency tree prettifier)"
 
 def get_javas():
     pacman = subprocess.check_output(['pacman', '-Ss', 'java'])
     lst = pacman.decode('utf-8').split("\n")
     lst = list(filter(lambda x: re.search("(jdk|jre)[0-9]*-openjdk ", x) != None, lst))
     lst = list(map(lambda x: x.replace("extra/", "").split(" ", 1)[0], lst))
-    lst.append(maven)
     return lst
 
 def install(program):
     subprocess.run(['sudo', 'pacman', '-S', '--needed', program])
 
-
 def install_maven():
-    install(maven)
+    install("maven")
     add_alias("alias mci=\\'mvn clean install\\'")
+
+def install_mvntree():
+    install("git")
+    home = str(Path.home())
+    subprocess.run(['mkdir', '-p', home + '/code'])
+    subprocess.run(['git', 'clone', 'https://github.com/sjoblomj/mvntree.git'], cwd=(home + '/code'))
+    zshrc_file = str(Path.home()) + '/.zshrc'
+    with open(zshrc_file, 'a+') as f:
+        f.seek(0)
+        content = f.read()
+        source_text = "source ~/code/mvntree/.mvntree"
+        if source_text not in content:
+            add_line_to_file(f, source_text)
 
 
 def add_line_to_file(f, line):
@@ -40,6 +53,9 @@ def add_alias(alias):
 
 def initial_menu():
     lst = get_javas()
+    lst.append(maven)
+    lst.append(mvntree)
+    lst = list(filter(lambda x: x not in installed_programs, lst))
     menu = SelectionMenu(lst, "Install Java?")
     menu.show()
 
@@ -47,8 +63,12 @@ def initial_menu():
         program = lst[menu.selected_option]
         if program == maven:
             install_maven()
+        elif program == mvntree:
+            install_mvntree()
         else:
             install(program)
+
+        installed_programs.append(program)
         initial_menu()
     return()
 
