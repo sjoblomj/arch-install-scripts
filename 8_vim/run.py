699f884f3d2822e3c9d88a1030ed9f5b26fbe76a
Johan Sjöblom <sjoblomj88@gmail.com>
2022-03-22 00:25:23 +0100

    Adding vimrc 🖋️

---
+++ 8_vim/run.py
@@ -0,0 +1,19 @@
+from cursesmenu import *
+from cursesmenu.items import *
+import subprocess
+
+def install_vimrc():
+    subprocess.run(['sh', '-c', '8_vim/vimrc.sh'])
+
+
+def initial_menu():
+    items = list()
+    items.append("Install and configure vimrc (Tweaked vim)")
+
+    menu = SelectionMenu(items, "Install vimrc?")
+    menu.show()
+    menu.exit()
+    if menu.selected_option < len(items):
+        install_vimrc()
+
+initial_menu()
