4b5c008db2dc67082d3fc6b7b716b74df928ae6a
Johan Sjöblom <sjoblomj88@gmail.com>
2022-06-10 17:48:42 +0200

    Adding new keybinding 🔑

--- 9_config/add_keybindings.sh
+++ 9_config/add_keybindings.sh
@@ -3,7 +3,10 @@ targetfile="$HOME/.config/openbox/rc.xml"
 if [ -f $targetfile ] && ! grep -q "keybind key=\"S-A-Tab\"" $targetfile ; then
 	awk -i inplace '
 	{
-		if ($0 ~ "keybind key=\"A-Tab\"") {
+        if ($0 ~ "keybind key=\"C-space\"") {
+            gsub("C-space", "C-A-space", $0);
+        }
+        if ($0 ~ "keybind key=\"A-Tab\"") {
 			print "    <keybind key=\"S-A-Tab\">";
 			print "      <action name=\"PreviousWindow\">";
 			print "        <allDesktops>yes</allDesktops>";
