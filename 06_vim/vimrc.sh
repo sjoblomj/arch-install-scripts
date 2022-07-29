40adf0164f5b8213359ee8efdbc00c8a790414ec
Johan Sjöblom <sjoblomj88@gmail.com>
2022-07-29 12:27:09 +0200

    Renaming directories 🌀

--- 06_vim/vimrc.sh
+++ 06_vim/vimrc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pacman -S --needed git vim
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-cp 05_vim/my_configs ~/.vim_runtime
+cp 06_vim/my_configs ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
