0203ff6948cb6f3ef9877c370c68e08149b60623
Johan Sjöblom <sjoblomj88@gmail.com>
2023-04-04 23:05:45 +0200

    Removing excessive file paths 🦮

--- 04_browser/arkenfox.sh
+++ 04_browser/arkenfox.sh
@@ -8,8 +8,8 @@ mkdir -p ~/.config/arkenfox
 cd ~/bin/arkenfox
 git clone https://github.com/arkenfox/user.js.git
 
-cp ~/code/arch-install-scripts/04_browser/user-overrides.js ~/bin/arkenfox/user.js/user-overrides.js
-cp ~/code/arch-install-scripts/04_browser/arkenfox-update.sh ~/.config/arkenfox/arkenfox-update.sh
+cp user-overrides.js ~/bin/arkenfox/user.js/user-overrides.js
+cp arkenfox-update.sh ~/.config/arkenfox/arkenfox-update.sh
 chmod +x ~/.config/arkenfox/arkenfox-update.sh
 
 if ! grep -sq "arkenfox-update.sh" ~/.zshrc ; then
