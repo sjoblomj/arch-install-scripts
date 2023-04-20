e6562c983d9742e302719d2975c3ecdb16332c64
Johan Sjöblom <sjoblomj88@gmail.com>
2023-04-20 17:31:06 +0200

    Adding latex directory 🤼‍♀️

---
+++ 12_latex/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+title="Install PDF tools and typesetting systems?"
+alt1="Install TeX Live (LaTeX etc)"
+alt2="Install mupdf-tools"
+esc="Cancel"
+alts="${alt1}\n${alt2}\n${esc}"
+
+while true; do
+	if [ "${alts}" = "${esc}" ]; then
+	    res="${esc}"
+	else
+	    res=$(printf "${alts}" | fzf --tac --margin=4 --border --border-label="${title}")
+	fi
+
+
+	if [ "${res}" = "${alt1}" ]; then
+	    alts=$(echo "${alts}" | sed "s/\\${alt1}\\\\n//g")
+	    ./install.sh texlive-core texlive-latexextra
+    elif [ "${res}" = "${alt2}" ]; then
+	    alts=$(echo "${alts}" | sed "s/\\${alt2}\\\\n//g")
+	    ./install.sh mupdf-tools
+	else
+	    exit 0
+	fi
+done
