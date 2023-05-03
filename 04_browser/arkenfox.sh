91b60b41f9a0ef5b744268aa17aaa3380eb8bc36
Johan Sjöblom <sjoblomj88@gmail.com>
2023-05-03 20:30:01 +0200

    Going to the rigth path 🚷

--- 04_browser/arkenfox.sh
+++ 04_browser/arkenfox.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+prevdir=$(pwd)
 firefox addons.html
 
 sudo pacman -S --needed git
+
 mkdir -p $HOME/bin/arkenfox
 mkdir -p $HOME/.config/arkenfox
 cd $HOME/bin/arkenfox
 git clone https://github.com/arkenfox/user.js.git
+cd "$prevdir"
 
 cp user-overrides.js $HOME/bin/arkenfox/user.js/user-overrides.js
 cp arkenfox-update.sh $HOME/.config/arkenfox/arkenfox-update.sh
