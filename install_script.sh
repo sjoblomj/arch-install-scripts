2c456d2e46ae54548562a55149780a6be516534c
Johan Sjöblom <sjoblomj88@gmail.com>
2023-05-03 20:29:26 +0200

    Reading right file 🔎

--- install_script.sh
+++ install_script.sh
@@ -10,10 +10,10 @@ mkdir -p $HOME/bin/fzf
 git clone https://github.com/junegunn/fzf.git $HOME/bin/fzf
 cd $HOME/bin/fzf
 ./install
+source $HOME/.fzf.zsh
 
 mkdir -p $HOME/code/arch-install-scripts
 git clone https://github.com/sjoblomj/arch-install-scripts $HOME/code/arch-install-scripts
 cd $HOME/code/arch-install-scripts
 
-source $HOME/.zshrc
 ./find_scripts.sh
