453a03c858d7ac9e25279a9824dd174fbfc7358a
Johan Sjöblom <sjoblomj88@gmail.com>
2023-05-04 08:47:49 +0200

    Adding Run Program to menu 🏃‍♀️

--- 00_config/add_keybindings.sh
+++ 00_config/add_keybindings.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+restartob=0
 targetfile="$HOME/.config/openbox/rc.xml"
 if [ -f $targetfile ] && ! grep -q "keybind key=\"S-A-Tab\"" $targetfile ; then
 	awk -i inplace '
 	{
-        if ($0 ~ "keybind key=\"S-A-Return\"") {
-            gsub("C-space", "W-Return", $0);
-        }
-         if ($0 ~ "keybind key=\"C-space\"") {
-            gsub("C-space", "C-A-space", $0);
-        }
-        if ($0 ~ "keybind key=\"A-Tab\"") {
+		if ($0 ~ "keybind key=\"S-A-Return\"") {
+			gsub("C-space", "W-Return", $0);
+		}
+		 if ($0 ~ "keybind key=\"C-space\"") {
+			gsub("C-space", "C-A-space", $0);
+		}
+		if ($0 ~ "keybind key=\"A-Tab\"") {
 			print "    <keybind key=\"S-A-Tab\">";
 			print "      <action name=\"PreviousWindow\">";
 			print "        <allDesktops>yes</allDesktops>";
@@ -24,6 +25,28 @@ if [ -f $targetfile ] && ! grep -q "keybind key=\"S-A-Tab\"" $targetfile ; then
 		print $0;
 	}' $targetfile
 
-	openbox --restart
+	restartob=1
 fi
 
+targetfile="$HOME/.config/openbox/menu.xml"
+if [ -f $targetfile ] && ! grep -q "Run program..." $targetfile ; then
+	awk -i inplace '
+	{
+		if ($0 ~ "<separator label=\"ArchLabs\"/>") {
+			print "    <separator label=\"ArchLabs\"/>";
+			print "    <item label=\"Run program...\">";
+			print "      <action name=\"Execute\">";
+			print "        <command>rofi -show drun</command>";
+			print "      </action>";
+			print "    </item>";
+		} else {
+			print $0;
+		}
+	}' $targetfile
+
+	restartob=1
+fi
+
+if [ $restartob == 1 ]; then
+	openbox --restart
+fi
