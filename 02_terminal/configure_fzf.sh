4793af8eb64d8948c77e3481ddab266064df8b97
Johan Sjöblom <sjoblomj88@gmail.com>
2023-05-04 10:11:58 +0200

    Adding fzf to zshrc ➕

--- 02_terminal/configure_fzf.sh
+++ 02_terminal/configure_fzf.sh
@@ -3,6 +3,9 @@
 mkdir -p $HOME/.config/fzf
 cp fzf-update.sh $HOME/.config/fzf
 
+if ! grep -sq ".fzf.zsh" $HOME/.zshrc ; then
+    echo '[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh' >> $HOME/.zshrc
+fi
 if ! grep -sq "fzf-update.sh" $HOME/.zshrc ; then
     echo 'source $HOME/.config/fzf/fzf-update.sh' >> $HOME/.zshrc
 fi
