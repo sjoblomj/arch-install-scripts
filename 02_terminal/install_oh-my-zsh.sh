fd97ec535b4db96d35bc459743aa2d24f2322683
Johan Sjöblom <sjoblomj88@gmail.com>
2023-09-24 15:44:50 +0000

    Setting zsh as default; preventing bug 🐚

--- 02_terminal/install_oh-my-zsh.sh
+++ 02_terminal/install_oh-my-zsh.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+
+sudo pacman -S --needed zsh zsh-completions
+sudo chsh -s $(which zsh) # Change default shell for root user
+chsh -s $(which zsh)      # Change default shell for normal user
+
 sudo pacman -S --needed curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Modify bgnotify plugin to take icon command
 sed -i "s/notify-send \"\$1\" \"\$2\"$/notify-send \"\$1\" \"\$2\" --icon=\"\$3\"/g" $HOME/.oh-my-zsh/plugins/bgnotify/bgnotify.plugin.zsh
+# Modify bgnotify plugin to not set bgnotify_termid
+sed -i "s/^bgnotify_termid=/#bgnotify_termid=/" $HOME/.oh-my-zsh/plugins/bgnotify/bgnotify.plugin.zsh
 
 if ! grep -sq "bgnotify_formatted" $HOME/.zshrc ; then
     plugins="git bgnotify sudo"
