dc336f449503293b02432656be3827c87d4c44b1
Johan Sjöblom <sjoblomj88@gmail.com>
2023-11-08 20:52:19 +0100

    Moving icon copying to its own function 🛌

--- 04_browser/run.sh
+++ 04_browser/run.sh
@@ -2,17 +2,24 @@
 title="Install web browsers? (Select with Tab, Esc to quit)"
 selections=$(printf "firefox\nchromium\ntorbrowser-launcher" | fzf --multi --tac --margin=4 --border --border-label="${title}")
 
+add_icons_to_system() {
+    added=0
+    for s in $1; do
+        filename=${s%-*}
+        if [ ! -f "/usr/share/icons/hicolor/scalable/apps/${filename}.svg" ]; then
+            sudo cp "${filename}.svg" "/usr/share/icons/hicolor/scalable/apps/${filename}.svg"
+            added=1
+        fi
+    done
+    if [ $added -eq 1 ]; then
+        sudo gtk-update-icon-cache /usr/share/icons/hicolor/
+    fi
+}
+
 if [ "$selections" != "" ]; then
     sudo pacman -S --needed $selections
     if [[ "$selections" =~ "firefox" ]]; then
         ./arkenfox.sh
     fi
-
-    # Add svg icons to system if needed
-    for s in $selections; do
-        filename=${s%-*}
-        if [ ! -f "/usr/share/icons/hicolor/scalable/apps/${filename}.svg" ]; then
-            sudo cp "${filename}.svg" "/usr/share/icons/hicolor/scalable/apps/${filename}.svg"
-        fi
-    done
+    add_icons_to_system "$selections"
 fi
