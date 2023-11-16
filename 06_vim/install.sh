28db02952cfc366a24a877789ed7d76c5b6c221e
Johan Sjöblom <sjoblomj88@gmail.com>
2023-11-16 23:01:45 +0100

    Setting vi to lead to vim ✐

--- 06_vim/install.sh
+++ 06_vim/install.sh
@@ -3,3 +3,4 @@ sudo pacman -S --needed git vim
 git clone --depth=1 https://github.com/amix/vimrc.git $HOME/.vim_runtime
 cp my_configs.vim $HOME/.vim_runtime
 sh $HOME/.vim_runtime/install_awesome_vimrc.sh
+sudo ln -sf $(which vim) /usr/local/bin/vi
