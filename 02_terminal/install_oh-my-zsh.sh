c7aef1ae6700251e403042d5bf075e3eaa871d29
Johan Sjöblom <sjoblomj88@gmail.com>
2023-11-24 18:03:42 +0100

    bgnotify icon is now part of ohmyzsh. Sourcing function from there 🇿

--- 02_terminal/install_oh-my-zsh.sh
+++ 02_terminal/install_oh-my-zsh.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-sudo pacman -S --needed curl
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Modify bgnotify plugin to take icon command
-sed -i "s/notify-send \"\$1\" \"\$2\"$/notify-send \"\$1\" \"\$2\" --icon=\"\$3\"/g" $HOME/.oh-my-zsh/plugins/bgnotify/bgnotify.plugin.zsh
+sudo pacman -S --needed zsh zsh-completions
+if [[ -z $ZSH ]]; then
+  sudo chsh -s $(which zsh) # Change default shell for root user
+  chsh -s $(which zsh)      # Change default shell for normal user
 
-if ! grep -sq "bgnotify_formatted" $HOME/.zshrc ; then
+  sudo pacman -S --needed curl
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+  iconpath='.local/share/icons/hicolor/64x64/actions'
+  mkdir -p "$HOME/$iconpath"
+  cp {success,fail}.png "$HOME/$iconpath"
+
+  if ! grep -sq "bgnotify_formatted" $HOME/.zshrc ; then
     plugins="git bgnotify sudo"
-    # Set bgnotify custom command and enable more plugins
-    sed -i "s/^plugins=(.*)/function bgnotify_formatted {\n  ## \$1=exit_status, \$2=command, \$3=elapsed_time\n  [ \$1 -eq 0 ] \&\& title=\"Great success\" || title=\"Command failed\"\n  [ \$1 -eq 0 ] \&\& icon=\"\$HOME\/code\/arch-install-scripts\/02_terminal\/success.png\" || icon=\"\$HOME\/code\/arch-install-scripts\/02_terminal\/fail.png\"\n  bgnotify \"\$title - took \${3}s\" \"\$2\" \"\$icon\";\n}\nplugins=($plugins)/g" $HOME/.zshrc
+    # Use the bgnotify custom command from the bgnotify readme, but modify it
+    bgnotify_func=$(cat $HOME/.oh-my-zsh/plugins/bgnotify/README.md | awk 'BEGIN{In_function = 0}{if ($0 == "function bgnotify_formatted {") In_function = 1; if (In_function) print $0; if ($0 == "}") In_function = 0}' | sed "s|Holy Smokes Batman|Great success|; s|Holy Graf Zeppelin|Command failed|; s|\$HOME/icons|\$HOME/$iconpath|g")
+
+    awk -v plugins="$plugins" -v bgnotify_func="${bgnotify_func}" -i inplace '{if ($0 ~ /^plugins=(.*)$/) print bgnotify_func "\nplugins=(" plugins ")"; else print $0}' $HOME/.zshrc
+  fi
 fi
