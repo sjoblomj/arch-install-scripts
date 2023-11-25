2e024c9dc5d093301a608ff84e435ffbd1784bae
Johan Sjöblom <sjoblomj88@gmail.com>
2023-11-25 13:03:38 +0100

    Adding commit-hook 🪝

--- install_script.sh
+++ install_script.sh
@@ -6,6 +6,11 @@ sudo pacman-key --populate archlinux
 sudo pacman -Suy
 sudo pacman -S --needed git
 
+if [ -d ".git/hooks" ]; then
+  cp commit-msg .git/hooks
+  chmod +x .git/hooks/commit-msg
+fi
+
 mkdir -p $HOME/bin/fzf
 git clone https://github.com/junegunn/fzf.git $HOME/bin/fzf
 cd $HOME/bin/fzf
