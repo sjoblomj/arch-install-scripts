46f27fd448170577f85e91d507cf3cab37ad5187
Johan Sjöblom <sjoblomj88@gmail.com>
2024-01-01 14:28:31 +0100

    Renaming directories, removing superfluous locales directory 🎆

--- 07_locales/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-title="Add more locales?"
-selection=$(cat /etc/locale.gen | grep -Po "^#\K\S*" | fzf --multi --layout=reverse --margin=4 --border --border-label="Add more locales? (Select with Tab)")
-
-if [ "$selection" != "" ]; then
-    ./add_locales.sh "$selection"
-fi
