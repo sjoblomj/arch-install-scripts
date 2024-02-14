2f9530daa9f29254e8baaa0c5e5169c21f6dd713
Johan Sjöblom <sjoblom.johan@gmail.com>
2024-02-14 20:03:41 +0100

    Using right name of variable 📟

--- 01_utils/run.sh
+++ 01_utils/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 preview_dir="programs/"
 title="Choose programs to install (Select with Tab)"
-selection=$(ls "$preview_dir" | fzf --multi --layout=reverse --margin=4 --border --border-label="${title}" --preview "cat ${preview_dir}{}" --preview-window right:wrap)
+selections=$(ls "$preview_dir" | fzf --multi --layout=reverse --margin=4 --border --border-label="${title}" --preview "cat ${preview_dir}{}" --preview-window right:wrap)
 
-if [ "$selection" != "" ]; then
-    sudo pacman -S --needed $selection
+if [ "$selections" != "" ]; then
+    sudo pacman -S --needed $selections
     if [[ "$selections" =~ "vlc" ]]; then
         sudo pacman -S --needed qt5-wayland
         mkdir -p $HOME.local/share/icons/hicolor/scalable/apps
@@ -23,7 +23,7 @@ if [ "$selection" != "" ]; then
             sudo cp /usr/share/applications/org.gnome.Evince.desktop /usr/share/applications/evince.desktop
         fi
     fi
-    if [[ "$selection" =~ "transmission-qt" ]]; then
+    if [[ "$selections" =~ "transmission-qt" ]]; then
         curl https://upload.wikimedia.org/wikipedia/commons/4/46/Transmission_Icon.svg -o /usr/share/icons/hicolor/scalable/apps/transmission.svg
         sudo curl https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Transmission_Icon.svg/48px-Transmission_Icon.svg.png -o /usr/share/icons/hicolor/48x48/apps/transmission.png
         sudo gtk-update-icon-cache /usr/share/icons/hicolor
