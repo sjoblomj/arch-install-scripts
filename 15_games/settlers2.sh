fa4676ad98f882d483ec885e8b87c00facc96c9e
Johan Sjöblom <sjoblom.johan@gmail.com>
2024-02-19 12:19:34 +0100

    Exiting properly 🆘

--- 15_games/settlers2.sh
+++ 15_games/settlers2.sh
@@ -8,7 +8,7 @@ if [ ! -d $HOME/games/settlers2 ]; then
     url=$(curl -s https://www.siedler25.org/index.php\?com\=dynamic\&mod\=2\&lang\=en\&PHPSESSID\=li5ljhjjcqeu02md0k5a1ivqhm | awk 'BEGIN{Found_stable = 0}{if ($0 ~ "Current Stable Version") Found_stable = 1; if ($0 ~ "a href=\"https" && $0 ~ "linux") { gsub(/.*a href="/, "", $0); gsub(/".*/, "", $0); print $0; exit 0;}}')
     if [ -z "$url" ]; then
         echo "Failed to fetch URL to download from!"
-        return 1
+        exit 1
     fi
     curl "$url" -o /tmp/rttr.tar.bz2
     tar xf /tmp/rttr.tar.bz2 --strip-components=1 --directory=$HOME/games/settlers2
